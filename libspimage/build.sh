d1d71ef62112a3d28372bbbcf5c08359d218cfe2
Filipe Maia <filipe.c.maia@gmail.com>
2019-02-20 09:27:00 +0800

    Disable CUDA

--- libspimage/build.sh
+++ libspimage/build.sh
@@ -1,5 +1,6 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX}
-make
-make install
+cmake .. -DUSE_CUDA=OFF \
+	    -DCMAKE_BUILD_TYPE=Release \
+	    -DCMAKE_PREFIX_PATH=${PREFIX} \
+	    -DCMAKE_INSTALL_PREFIX=${PREFIX}
